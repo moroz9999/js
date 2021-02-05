4fdcb25ba1179aaa9cc77d1c40d026286122fff6
Moroz <moroz9999@mail.ru>
2021-02-05 13:48:45 +0300

    real codes for lesson 04 added

--- 04_JS_01.js
+++ 04_JS_01.js
@@ -1,1 +1,21 @@
-// Заглушка для задания 1, урок 4
+// Задание 1, урок 4
+// Функция, возвращающая объект с описанием состава числа
+function parseNum2Str(num) {
+  if (num < 0 || num > 999) {
+    console.log("Введенное число не принадлежит диапазону [0-999]")
+    return {};
+  }
+  let numbers = {
+    hundreds: '',
+    tens: '',
+    units: '',
+  }
+  numbers.hundreds = parseInt(num / 100, 10);
+  numbers.tens = parseInt((num % 100) / 10, 10);
+  numbers.units = num % 10;
+  return numbers;
+}
+
+let n = 437;
+// let n = prompt("Введите целое число от 0 до 999:");
+console.log(parseNum2Str(n));
--- 04_JS_02.js
+++ 04_JS_02.js
@@ -1,1 +1,65 @@
-// Заглушка для задания 2, урок 4
+// Задание 2, урок 4
+
+// Корзина, вариант 2. 
+// Наблюдается избыточность кода, но вроде как это в текущей ситуации неизбежно
+// Зато так уже веселее выглядит :)
+const cart = [
+  {
+    product: 'scissors',
+    price: 350,
+    quantity: 2,
+
+    mySum() {
+      return this.price * this.quantity;
+      },
+  },
+  {
+    product: 'paints',
+    price: 200,
+    quantity: 1,
+    
+    mySum() {
+      return this.price * this.quantity;
+      },
+  },
+  {
+    product: 'paper',
+    price: 100,
+    quantity: 3,
+    
+    mySum() {
+      return this.price * this.quantity;
+      },
+  }
+]
+let sum = 0;
+for(i = 0; i < cart.length; i++) {
+  sum += cart[i].mySum();
+}
+console.log(sum);
+
+
+// Корзина, вариант 1 (без дублирования кода, но и без методов)
+// const cart = [
+//   {
+//     product: 'scissors',
+//     price: 350,
+//     quantity: 2
+//   },
+//   {
+//     product: 'paints',
+//     price: 200,
+//     quantity: 1
+//   },
+//   {
+//     product: 'paper',
+//     price: 100,
+//     quantity: 3
+//   }
+// ]
+// let sum = 0;
+// for(i = 0; i < cart.length; i++) {
+//   sum_i = cart[i].price * cart[i].quantity;
+//   sum += sum_i;
+// }
+// console.log(sum);
