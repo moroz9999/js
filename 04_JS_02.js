bd0dd13a330a8b11d756e7c86fb929e7d570d4fb
Moroz <moroz9999@mail.ru>
2021-02-10 12:45:33 +0300

    updated code for lesson 04 added task2

--- 04_JS_02.js
+++ 04_JS_02.js
@@ -1,5 +1,35 @@
 // Задание 2, урок 4
 
+// Корзина, вариант 3. Исправлено в соответствии с рекомендациями.
+myCart = {  
+  products: [
+    {
+      product: 'scissors',
+      price: 350,
+      quantity: 2,
+    },
+    {
+      product: 'paints',
+      price: 200,
+      quantity: 1,
+    },
+    {
+      product: 'paper',
+      price: 100,
+      quantity: 3
+    },
+  ],
+  getSum(){
+    let sum = 0;
+    for(i = 0; i < this.products.length; i++) {
+      sum += this.products[i].price * this.products[i].quantity;
+   }
+    return sum;
+  },
+}
+
+console.log(myCart.getSum());
+
 // Корзина, вариант 2. 
 // Наблюдается избыточность кода, но вроде как это в текущей ситуации неизбежно
 // Зато так уже веселее выглядит :)
